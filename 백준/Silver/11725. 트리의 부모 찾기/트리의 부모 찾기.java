9562ea049df13c93cd50b8c1aa4cca01224608cc
Moon Jin Ho <117807455+moonjinho99@users.noreply.github.com>
2025-03-27 14:46:30 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1008 ms, Memory: 70876 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -4,48 +4,56 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static List<Integer>[] tree;
-    static int[] parent;
+
+    static List<Integer>[] node;
+    static int[] parents;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int cnt = Integer.parseInt(br.readLine());
 
-        tree = new ArrayList[cnt + 1];
-        for(int i=1; i<=cnt; i++){
-            tree[i] = new ArrayList<>();
+        parents = new int[cnt+1];
+        node = new ArrayList[cnt+1];
+        for(int i=1; i<=cnt; i++)
+        {
+            node[i] = new ArrayList<>();
         }
 
-        for(int i=1; i<cnt; i++){
+        for(int i=1; i<cnt; i++)
+        {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int num1 = Integer.parseInt(st.nextToken());
-            int num2 = Integer.parseInt(st.nextToken());
-            tree[num1].add(num2);
-            tree[num2].add(num1);
+            int node1 = Integer.parseInt(st.nextToken());
+            int node2 = Integer.parseInt(st.nextToken());
+            node[node1].add(node2);
+            node[node2].add(node1);
         }
-
-        parent = new int[cnt + 1];
         bfs(1);
 
-        for(int i=2; i<=cnt; i++){
-            System.out.println(parent[i]);
+        for(int i=2; i<=cnt; i++)
+        {
+            System.out.println(parents[i]);
         }
+
         br.close();
     }
 
-    public static void bfs(int start)
+    private static void bfs(int start)
     {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
 
-        while (!queue.isEmpty()){
-            int node = queue.poll();
-            for (int next : tree[node]){
-                if(parent[next] == 0){
-                    parent[next] = node;
+        while(!queue.isEmpty())
+        {
+            int current = queue.poll();
+            for(int next : node[current])
+            {
+                if(parents[next] == 0)
+                {
+                    parents[next] = current;
                     queue.add(next);
                 }
             }
         }
     }
+
 }
