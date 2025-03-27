81eb520df2e59a860ac75c6ac4a0255929108576
Moon Jin Ho <117807455+moonjinho99@users.noreply.github.com>
2025-03-27 15:59:00 +0900

    [Silver II] Title: DFS와 BFS, Time: 288 ms, Memory: 21104 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,112 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collector;
 
 public class Main {
 
-	static boolean visited[];
-	static int[][] graph;
-	static int N;
-	static int M;
-	
-	
-	public static void main(String[] args) throws IOException
-	{					
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		int start = Integer.parseInt(st.nextToken());
-		
-		visited = new boolean[N+1];
-		graph = new int[N+1][N+1];
-		
-		for(int i=0; i<M; i++)
-		{
-			StringTokenizer inputNode = new StringTokenizer(br.readLine()," ");
-			int link1 = Integer.parseInt(inputNode.nextToken());
-			int link2 = Integer.parseInt(inputNode.nextToken());
-			
-			graph[link1][link2] = graph[link2][link1] = 1;
-		}
-		
-		dfs(start);
-		
-		visited = new boolean[N+1];
-		System.out.println();
-		
-		bfs(start);
+    static List<Integer>[] node;
+    static boolean visited[];
 
-	}				
-	
-	
-//	public static void dfs(int start)
-//	{
-//		Stack<Integer> stack = new Stack<Integer>();
-//		stack.push(start);
-//		
-//		while(!stack.isEmpty())
-//		{
-//			int node = stack.pop();			
-//			System.out.print(node + " ");
-//			
-//			for(int i=0; i<graph[node].length; i++)
-//			{			
-//				if(!visited[node] && graph[node][i] != 0)
-//				{					
-//					visited[i] = true;
-//					stack.push(i);
-//				}
-//			}
-//		}
-//	}
-//	
-	
-	public static void dfs(int start)
-	{
-		visited[start] = true;
-		System.out.print(start + " ");
-			
-		for(int i=0; i<graph[start].length; i++)
-		{			
-			if(!visited[i] && graph[start][i] != 0)
-			{					
-				dfs(i);
-			}
-		}
-		
-	}
-	
-	
-	
-	public static void bfs(int start)
-	{
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.offer(start);
-		visited[start] = true;
-				
-		while(!queue.isEmpty())
-		{
-			int node = queue.poll();
-			System.out.print(node + " ");
-			
-			for(int i=0; i<graph[node].length; i++)
-			{
-				if(!visited[i] && graph[node][i] != 0)
-				{
-					visited[i] = true;
-					queue.offer(i);
-				}
-			}
-			
-		}
-		
-	}
-	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        node = new ArrayList[N+1];
+        for(int i=1; i<=N; i++)
+        {
+            node[i] = new ArrayList<>();
+        }
+
+        for(int i=1; i<=M; i++)
+        {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st2.nextToken());
+            int node2 = Integer.parseInt(st2.nextToken());
+            node[node1].add(node2);
+            node[node2].add(node1);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(node[i]);
+        }
+
+        visited = new boolean[N+1];
+        dfs(V);
+
+        visited = new boolean[N+1];
+        System.out.println();
+        bfs(V);
+
+        br.close();
+    }
+
+    private static void dfs(int start)
+    {
+        System.out.print(start+" ");
+        visited[start] = true;
+        for(int next : node[start])
+        {
+            if(!visited[next])
+                dfs(next);
+        }
+    }
+
+    private static void bfs(int start)
+    {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+
+        while(!queue.isEmpty())
+        {
+            int current = queue.poll();
+            System.out.print(current+" ");
+            for(int next : node[current])
+            {
+                if(!visited[next])
+                {
+                    visited[next] = true;
+                    queue.add(next);
+                }
+            }
+        }
+    }
+
 }
