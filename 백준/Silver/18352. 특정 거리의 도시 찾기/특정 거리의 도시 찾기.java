ee7396daa6c273c02b5577b890b1beaed465f1f7
Moon Jin Ho <117807455+moonjinho99@users.noreply.github.com>
2025-03-31 11:41:09 +0900

    Update 특정 거리의 도시 찾기.java

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,63 +1,81 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
     static List<Integer>[] cities;
-    static int[] load; // 거리 배열
+    static boolean visited[];
+    static int[] load;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(st.nextToken()); // 도시 개수
-        int M = Integer.parseInt(st.nextToken()); // 도로 개수
-        int K = Integer.parseInt(st.nextToken()); // 거리 정보
-        int X = Integer.parseInt(st.nextToken()); // 출발 도시
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+        int K = Integer.parseInt(input[2]);
+        int X = Integer.parseInt(input[3]);
 
-        cities = new ArrayList[N + 1];
-        load = new int[N + 1];
-        Arrays.fill(load, -1); // 방문하지 않은 도시의 거리 값은 -1로 설정
-
-        for (int i = 1; i <= N; i++) {
+        load = new int[N+1];
+        visited = new boolean[N+1];
+        cities = new ArrayList[N+1];
+        for(int i=1; i<N+1; i++)
+        {
             cities[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int city1 = Integer.parseInt(st.nextToken());
-            int city2 = Integer.parseInt(st.nextToken());
-            cities[city1].add(city2); // 단방향 그래프
+        for(int i=0; i<M; i++)
+        {
+            String[] city = br.readLine().split(" ");
+            int city1 = Integer.parseInt(city[0]);
+            int city2 = Integer.parseInt(city[1]);
+
+            cities[city1].add(city2);
         }
 
         bfs(X);
 
-        // 결과 저장
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            if (load[i] == K) {
-                sb.append(i).append("\n");
+        int printCnt = 0;
+        for(int i=1; i<=N; i++)
+        {
+            if(load[i] == K){
+                bw.write(i + "\n");
+                printCnt++;
             }
         }
 
-        // 결과 출력
-        System.out.print(sb.length() == 0 ? "-1" : sb.toString());
+        if(printCnt == 0)
+            bw.write("-1\n");
+
+        bw.flush();
+        bw.close();
         br.close();
     }
 
-    private static void bfs(int start) {
+
+    private static void bfs(int start)
+    {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
-        load[start] = 0; // 시작 도시의 거리 = 0
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty())
+        {
             int city = queue.poll();
-            for (int next : cities[city]) {
-                if (load[next] == -1) { // 방문하지 않은 도시만 탐색
+            visited[city] = true;
+            for(int next : cities[city])
+            {
+                if(!visited[next])
+                {
                     queue.add(next);
-                    load[next] = load[city] + 1; // 거리 갱신
+                    if(load[next] == 0)
+                        load[next] = load[city]+1;
                 }
+
             }
+
         }
+
     }
 }
